ecd5089263280636e21ddc15caaa6845a9cd7cfb
Monika Majer <majer_monika@interia.pl>
2018-12-05 17:25:16 +0100

    Add js code

--- js/index.js
+++ js/index.js
@@ -3,13 +3,13 @@ function Phone(brand, price, color) {
   this.price = price;
   this.color = color;
 }
-Phone.prototype.printInfo = function() {
+Phone.prototype.printInfo = function () {
   console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 }
 
-var SamsungGalaxyS6 = new Phone("Samsung", 2000, "red");
-var iPhone6S = new Phone("Apple", 2250, "silver");
-var OnePlusOne = new Phone("OnePlus One", 1500, "black");
+var SamsungGalaxyS6 = new Phone("Samsung", 1748, "gold");
+var iPhone6S = new Phone("Apple", 1948, "pink");
+var OnePlusOne = new Phone("OnePlus One", 1299, "black");
 
 SamsungGalaxyS6.printInfo();
 iPhone6S.printInfo();
